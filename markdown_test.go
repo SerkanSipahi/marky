b9ac0302f089c62e02de9f89fe2b4fe5699d5fd4
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-11-19 22:16:48 +0100

    Add unit tests

--- markdown_test.go
+++ markdown_test.go
@@ -22,3 +22,108 @@ func TestNewMarkdown(t *testing.T) {
 	}
 
 }
+
+func TestCreateHeaderTag(t *testing.T) {
+
+	// without newLine option and limited h tag size according specification
+
+	expectedTag := "<h1>Hello World</h1>"
+	createdTags := marky.CreateHeaderTag("Hello World", 1, false)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+	expectedTag = "<h6>Hello World</h6>"
+	createdTags = marky.CreateHeaderTag("Hello World", 6, false)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+	expectedTag = "<h6>Hello World</h6>"
+	createdTags = marky.CreateHeaderTag("Hello World", 12, false)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+	// with newLine option
+	expectedTag = "<h3>Hello World</h3>\n"
+	createdTags = marky.CreateHeaderTag("Hello World", 3, true)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+}
+
+func TestCreatePTag(t *testing.T) {
+
+	// without newLine option
+	expectedTag := "<p>Hello World</p>"
+	createdTags := marky.CreatePTag("Hello World", false)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+	// with newLine option
+	expectedTag = "<p>Hello World</p>\n"
+	createdTags = marky.CreatePTag("Hello World", true)
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+}
+
+func TestCreateLinkTag(t *testing.T) {
+
+	expectedTag := "<a href='http://example.com'>Hello World</a>"
+	createdTags := marky.CreateLinkTag("Hello World", "http://example.com")
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+}
+
+func TestCreateEmTag(t *testing.T) {
+
+	expectedTag := "<em>Hello World</em>"
+	createdTags := marky.CreateEmTag("Hello World")
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+}
+
+func TestCreateStrongTag(t *testing.T) {
+
+	expectedTag := "<strong>Hello World</strong>"
+	createdTags := marky.CreateStrongTag("Hello World")
+	if createdTags != expectedTag {
+		t.Error(
+			"expected", expectedTag,
+			"got", createdTags,
+		)
+	}
+
+}
